7c3042bf13e299e3c61c14770c8d8c30e17cd92e
Vaaragh <vaarghl@gmail.com>
2022-06-17 00:32:58 +0200

    feat: add full register functionality, small fix to regex enum

--- src/Enums/RegexP.java
+++ src/Enums/RegexP.java
@@ -5,16 +5,26 @@ public enum RegexP {
 	ID("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"),
 	NAME("([A-Z]{1}[a-z]+ *)+"),
 	DATE("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))"),
-	TEXT("([A-Z ,.-&a-z0-9])*"),
+	TEXT("([A-Z ,.-a-z0-9])*"),
 	YEAR("([1-9]{1}[0-9]{3}"),
 	NUMBER("([0-9]+[1-9]*)"),
 	PHONE("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$"),
 	TIME("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"),
 	JMBG("[0-9]{13}"),
-	TEMP("[a-z]+"),
+	TEMP("[A-Z a-z]+"),
 	
 	;
 	
+	//ID id
+	//NAME firstName, lastName
+	//DATE LocalDate
+	//TEXT description, address
+	//YEAR year
+	//NUMBER price/wage
+	//PHONE phone
+	//TIME localTime
+	//JMBG jmbg
+	//TEMP testing
 	
 	public final String pattern;
 	
--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -4,8 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
 
 import enums.RegexP;
 import managers.AdminManager;
@@ -28,15 +31,66 @@ public class RegisterController {
 	}
 	
 	public void initRegistrationChecker() {
-		RegisterController rc = this;
 		this.view.getRegisterBtn().addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				Pattern idPattern = Pattern.compile(RegexP.ID.pattern);
-				Matcher matcher = idPattern.matcher(view.getIdTextField().getText());
+				ArrayList<String> emptyCheckList = new ArrayList<String>();
+				
+				String id = view.getIdTextField().getText().trim();
+				String firstName = view.getFirstNameTextField().getText().trim();
+				String lastName = view.getLastNameTextField().getText().trim();
+				String jmbg = view.getJmbgTextField().getText().trim();
+				String address = view.getAdressTextField().getText().trim();
+				String gender = view.getGenderComboBox().getSelectedItem().toString().trim();
+				String wage = view.getWageTextField().getText().trim();
+				String userName = view.getUsernameTextField().getText().trim();
+				String password = new String(view.getPasswordTextField().getPassword()).trim();
+				
+				emptyCheckList.addAll(Arrays.asList(address, lastName, firstName, gender, id, "false", jmbg, password, userName, wage));
+				
+				if (emptyCheckList.contains("")) {
+					JOptionPane.showMessageDialog(null,"All fields are required.", "Error", JOptionPane.WARNING_MESSAGE);				
+				} else {
+					
+					Pattern idPattern = Pattern.compile(RegexP.ID.pattern);
+					Pattern namePattern = Pattern.compile(RegexP.NAME.pattern);
+					Pattern jmbgPattern = Pattern.compile(RegexP.JMBG.pattern);
+					Pattern textPattern = Pattern.compile(RegexP.TEXT.pattern);
+					Pattern wagePattern = Pattern.compile(RegexP.NUMBER.pattern);
+				
+					if (!idPattern.matcher(id).find() ||
+						!namePattern.matcher(firstName).find() ||
+						!namePattern.matcher(lastName).find() ||
+						!jmbgPattern.matcher(jmbg).find() ||
+						!textPattern.matcher(address).find() ||
+						!wagePattern.matcher(wage).find() ||
+						!textPattern.matcher(userName).find() ||
+						!textPattern.matcher(password).find()) {
+						
+						JOptionPane.showMessageDialog(null,"One or more mismatches", "Error", JOptionPane.WARNING_MESSAGE);
+					} else {
+						StringBuilder sb = new StringBuilder();
+						for (String s: emptyCheckList) {
+							sb.append(s + "|");
+						}
+						try {
+							if (adminModel.createAdmin(sb.toString().split("\\|"))) {
+								JOptionPane.showMessageDialog(null,"Welcome "+ userName + ", Our new Lord Admin", "Success!", JOptionPane.INFORMATION_MESSAGE);
+								view.getFrame().dispose();
+								view.getFrame().setVisible(false);
 
+							} else {
+								JOptionPane.showMessageDialog(null,"Info collision", "Error", JOptionPane.WARNING_MESSAGE);
+
+							}
+						} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+								| IOException e1) {
+							e1.printStackTrace();
+						}
+					}
+				}
 			}
 			
 		});
