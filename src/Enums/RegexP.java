5715eb7b1d1272cd59bb24cb99dcc591abe32964
Vaaragh <vaarghl@gmail.com>
2022-06-16 03:26:35 +0200

    fix: rename package, added regex bases

---
+++ src/Enums/RegexP.java
@@ -0,0 +1,25 @@
+package enums;
+
+public enum RegexP {
+	
+	ID("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"),
+	NAME("([A-Z]{1}[a-z]+ *)+"),
+	DATE("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))"),
+	TEXT("([A-Z ,.-&a-z0-9])*"),
+	YEAR("([1-9]{1}[0-9]{3}"),
+	NUMBER("([0-9]+[1-9]*)"),
+	PHONE("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$"),
+	TIME("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"),
+	JMBG("[0-9]{13}"),
+	TEMP("[a-z]+"),
+	
+	;
+	
+	
+	public final String pattern;
+	
+	private RegexP(String pattern) {
+		this.pattern = pattern;
+	}
+	
+}
